58540f4794c11576848cd82bb1d3e53195bdfd4e
urtesala <urtesaladziute@gmail.com>
2022-10-10 09:58:45 +0300

    defer, console log, example.js

---
+++ example.js
@@ -0,0 +1,26 @@
+
+
+// čia raom JS kodą
+5+5
+console.log(5+5)
+
+// sukuriam kintamaji
+// = priskiria desine puse kairei
+let vardas = 'Urte';
+
+// sukuriu h1 elementa
+const h1El = document.createElement('h1')
+
+// pridedu tekstine reiksme
+h1El.textContent = vardas
+
+document.body.append(h1El)
+
+// pakeisti dyti
+h1El.style.fontSize = '5rem';
+// vardas = 'Mike';
+// vardas = 500;
+// vardas = 'true'
+
+console.log (5+5);
+console.log(vardas);
